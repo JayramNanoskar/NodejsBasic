d8d1b5a4ac5e9c4fbc284987a2d652d91979a18f
JayramNanoskar <jayram.n978@gmail.com>
2020-11-02 20:01:44 +0530

    connecting through mongodb

---
+++ crudnode/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost/test';
+const app = express();
+
+mongoose.connect(url, { useNewUrlParser : true, useUnifiedTopology : true }) //establish a connection
+    .then(() => {
+        console.log("Connected to Database");
+        
+    })
+    .catch((err) => {
+        console.log("Not Connected to Database ERROR! ", err);
+    });
+
+const con = mongoose.connection; //to get hold on connection
+// console.log(con.port); shows default mongodb port 27017
+
+con.on('open',  () => { //event on connection
+    console.log("Connection is open!!");
+});
+
+app.listen(9000, () => { //creating a server
+    console.log("Server started !");
+});
